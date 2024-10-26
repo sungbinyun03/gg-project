45fe5b3cb6d6002f12016a7522ceee8ca42be5c3
jc0432 <110874284+jc0432@users.noreply.github.com>
2024-10-25 19:31:04 -0500

    pushing new branch

--- Round 1 - James.py
+++ Round 1 - James.py
@@ -9,19 +9,12 @@ from inflection import humanize, underscore
 import json 
 import csv 
 
-
-
 with open('gg2013answers.json', 'r') as f:
     answers = json.load(f)
-
-
-### Replace any portion of the word with the full nominee name 
-### Replace words with hope or wish or think 
 
 ## Pre-processing the data-set 
 with open('firstpass.json', 'r') as f:
     raw_tweet_data = json.load(f)
-
 
 processed_tweets = []
 for tweet in raw_tweet_data:
@@ -38,28 +31,17 @@ for tweet in raw_tweet_data:
     cleaned_tweet = " ".join(cleaned_tweet.split())
     processed_tweets.append(cleaned_tweet)
 
-
-# def create_nominee_regex(nominees_dict):
-#     all_nominees = set()
-#     for nominees in nominees_dict.values():
-#         all_nominees.update(nominees)
-    
-#     # Sort nominees by length (longest first) to avoid partial matches
-#     sorted_nominees = sorted(all_nominees, key=len, reverse=True)
-    
-#     # Escape special regex characters and create alternations
-#     escaped_nominees = [re.escape(nominee) for nominee in sorted_nominees]
-#     regex_pattern = '|'.join(escaped_nominees)
-    
-#     # Make it case insensitive and allow for partial word matches
-#     return rf'\b(?:{regex_pattern})\b'
-
 # Frames 
 pos_keywords = ['win', 'won', 'winner', 'awarded', 'wins']
-uncertin_keywords = ['hope', 'hoping', 'think', 'predict', 'prediction', 'maybe', 'could', 'should','dont', 'don''t']
+uncertin_keywords = ['hope', 'hoping', 'think', 'predict', 'prediction', 'maybe', 'could', ' believe','should','dont', 'don''t', 'will', 'who', 'want', 'always win', 'should']
+negative_keywords = ['didn''t', 'doesn''t', 'didnt', 'doesnt' , 'did not'] 
 pos_keyword_reex = r'\b(' + '|'.join(pos_keywords) + r')\b'
 uncertain_keyword_reex = r'\b(' + '|'.join(uncertin_keywords) + r')\b'
+negative_keyword_reex = r'\b(' + '|'.join(negative_keywords) + r')\b'
 # print(uncertain_keyword_reex)
+
+def normalize_text(text):
+    return re.sub(r'[^a-zA-Z0-9\s-]', '', text).lower()
 
 # make nominee re keywords 
 nominee_database  = {}
@@ -70,59 +52,58 @@ for category in answers['award_data'].keys():
             nominee_database[category] = answers['award_data'][category]['nominees']
         else:
             nominee_database[category] = answers['award_data'][category]['nominees']
+        for nominee in nominee_database[category]:
+            nominee_database[category] = [normalize_text(nominee) for nominee in nominee_database[category]]
+
 
 
 nominee_lists= [nominee_list for nominee_list in nominee_database.values()]
 nominee_seperated = [item for sublist in nominee_lists for item in sublist]
 nominee_reex = r'\b(' + '|'.join(nominee_seperated) + r')\b'
 
-
-
-def normalize_text(text):
-    return re.sub(r'[^a-zA-Z0-9\s]', '', text).lower()
-
 def fufills_pattern(normal_tweet):
-    weight = 0
-    if re.findall(pos_keyword_reex, normal_tweet, re.IGNORECASE): 
-        weight += 3 
-    if re.findall(uncertain_keyword_reex, normal_tweet, re.IGNORECASE):
-        weight -= 2.5
-    return [normal_tweet, weight]
+    weighted_scores = {}
+    for k in normal_tweet:
+        post_list = []  # Reset lists for each nominee
+        neg_list = []
+        uncertain_list = []
+        
+        # Process each tweet for the current nominee
+        for tweet in normal_tweet[k]:
+            if re.findall(pos_keyword_reex, tweet, re.IGNORECASE): 
+                post_list.append(tweet)
+            if re.findall(uncertain_keyword_reex, tweet, re.IGNORECASE):
+                neg_list.append(tweet)
+            if re.findall(negative_keyword_reex, tweet, re.IGNORECASE):
+                uncertain_list.append(tweet)
+        
+        # Calculate score for current nominee
+        weighted_scores[k] = (len(post_list) * 2) + (len(neg_list) * -.5) + (len(uncertain_list) * 1)
+    
+    return weighted_scores
+    
 
+def nominee_matcher(tweet_list):
+    nominee_dict = {}
+    for listt in nominee_lists:
+        for item in listt: 
+            nominee_dict[item] = []
 
-def nominee_matcher(filtered_tweets):
-    counter_dict = {}
-    count = 0
-    weight = 0 
-    for tweet in filtered_tweets:
+    for tweet in tweet_list:
         check = re.search(nominee_reex, tweet, re.IGNORECASE)
         if check:
-            count +=1 
-            # key is the award category
-            # value is the list of nomineees 
-            for k, v in nominee_database.items():
-                if check.group() in v:
-                    value = check.group() 
-                    counter_dict[value] = [k, count]
+            value = check.group().lower()
+            nominee_dict[value].append(normalize_text(tweet))
 
-    return(counter_dict)
-   
-    
-# First need to find the name of the actor / title 
-# Now, need to search for the number of occurences for this name in the filtered_tweet database 
-# Store it in some new datatype 
-# Once we have a list with the count, need to matchup the role of that indivudal with 
-
-    
-tweet_list = []
+    return(nominee_dict)
 
 def export_to_csv(nominee_counts):
     # Create a list to store rows
     rows = []
-    
     # Collect the rows from nominee_counts
-    for name, award_info in nominee_counts.items():
-        rows.append([award_info[0], name, award_info[1]])
+    for award in nominee_counts:
+        for nominee, score in nominee_counts[award]:
+            rows.append([award, nominee, score])
 
     # Create a DataFrame from the list
     df = pd.DataFrame(rows, columns=["Award", "Nominee", "Mentions"])
@@ -132,36 +113,22 @@ def export_to_csv(nominee_counts):
 
     return df  # Return the DataFrame for further processing
 
-def main(): 
-    for tweet in processed_tweets:
-        # print(tweet)
-        normal_tweet = normalize_text(tweet)
-        tweet_list.append(fufills_pattern(normal_tweet))
-    filtered_tweets = [tweet[0] for tweet in tweet_list if tweet[1] > 2]
-    mentions_unsorted = nominee_matcher(filtered_tweets)
-    df = export_to_csv(mentions_unsorted)
+def main():
+    # returns dict of nominees with all their corresponding tweets 
+    winners_dict = {}
+    nominee_matches = nominee_matcher(processed_tweets) 
+    weighted_dict = fufills_pattern(nominee_matches)
+    print(weighted_dict)
+    for award in nominee_database:
+        for nominee in weighted_dict:
+            if nominee in nominee_database[award]:
+                if award not in winners_dict:
+                    winners_dict[award] = []
+                winners_dict[award].append([nominee, weighted_dict[nominee]])
+    
+    df = export_to_csv(winners_dict)
     df_sorted = df.sort_values(by=["Award", "Mentions"], ascending=[True, False])
     df_sorted.to_csv('sorted_nominee_mentions.csv', index=False)
     print(df_sorted)
 
-
-
 main()
-
-
-
-
-
-# Look at indivudal tweet 
-# if it matches one of the nominee regex 
-# then create/add it to the dictionary with a count + 1 
-# The key should be the actor / movie 
-
-
-
-
-
-
-
-
-
